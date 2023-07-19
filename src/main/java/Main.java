be95ae9f180c72195d314a4b02fd8e781c2dba3e
telion85 <www.neon@mail.ru>
2023-07-19 15:19:21 +0500

    Java solution

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> numberList = new ArrayList<>(List.of(1, 3, 4, 5, 1, 5, 4));
+        Map<Integer, Integer> result = countNumbers(numberList);
+        System.out.println("result = " + result);
+    }
+
+    private static Map<Integer, Integer> countNumbers(List<Integer> numberList) {
+        Map<Integer, Integer> result = new HashMap<>();
+        for (int number : numberList) {
+            result.put(number, Optional.ofNullable(result.get(number)).orElse(0) + 1);
+        }
+        return result;
+    }
+}
